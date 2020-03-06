5e9a184eb377d69dcb6ec190244c1de977cf35a0
Alex Tripsas <alextripsas@gmail.com>
2020-03-06 16:14:55 +0100

    update/removal of submodule

--- yeti2WhiteOrangeRace.py
+++ yeti2WhiteOrangeRace.py
@@ -13,3 +13,56 @@ import picamera.array
 import cv2
 import numpy
 
+
+
+
+# Setup the ZeroBorg
+ZB = ZeroBorg.ZeroBorg()
+#ZB.i2cAddress = 0x44                  # Uncomment and change the value if you have changed the board address
+ZB.Init()
+#Safety stuff
+if not ZB.foundChip:
+    boards = ZeroBorg.ScanForZeroBorg()
+    if len(boards) == 0:
+        print 'No ZeroBorg found, check you are attached :)'
+    else:
+        print 'No ZeroBorg at address %02X, but we did find boards:' % (ZB.i2cAddress)
+        for board in boards:
+            print '    %02X (%d)' % (board, board)
+        print 'If you need to change the I�C address change the setup line so it is correct, e.g.'
+        print 'ZB.i2cAddress = 0x%02X' % (boards[0])
+    sys.exit()
+#ZB.SetEpoIgnore(True)                 # Uncomment to disable EPO latch, needed if you do not have a switch / jumper
+# Ensure the communications failsafe has been enabled!
+failsafe = False
+for i in range(5):
+    ZB.SetCommsFailsafe(True)
+    failsafe = ZB.GetCommsFailsafe()
+    if failsafe:
+        break
+if not failsafe:
+    print 'Board %02X failed to report in failsafe mode!' % (ZB.i2cAddress)
+    sys.exit()
+ZB.ResetEpo()
+
+# Power settings
+voltageIn = 8.4                         # Total battery voltage to the ZeroBorg (change to 9V if using a non-rechargeable battery)
+voltageOut = 6.0                        # Maximum motor voltage
+
+# Camera settings
+imageWidth  = 320                       # Camera image width
+imageHeight = 240                       # Camera image height
+frameRate = 10                          # Camera image capture frame rate
+
+# Auto drive settings
+autoZoneCount = 80                      # Number of detection zones, higher is more accurate
+autoMinimumMovement = 20                # Minimum movement detection before driving
+steeringGain = 4.0                      # Use to increase or decrease the amount of steering used
+flippedImage = True                     # True if the camera needs to be rotated
+showDebug = True                        # True to display detection values
+
+# Setup the power limits
+if voltageOut > voltageIn:
+    maxPower = 1.0
+else:
+    maxPower = voltageOut / float(voltageIn)
