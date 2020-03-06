ade99f4890188a1c1683e092e0a806e15b10d3d5
Alex Tripsas <alextripsas@gmail.com>
2020-03-06 15:31:48 +0100

    yetiborg examples and initial race code

---
+++ yeti2WhiteOrangeRace.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: Latin-1
+
+# Load library functions we want
+import time
+import os
+import sys
+import ZeroBorg
+import io
+import threading
+import picamera
+import picamera.array
+import cv2
+import numpy
+
