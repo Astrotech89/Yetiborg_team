5fe17da98b85c01fd117793a8fecf0536b0442a2
ipolito42 <phypolitopoulos@gmail.com>
2020-05-02 19:07:05 +0200

    for slopes > 45 deg
    Might work, needs testing

--- new_single_line_support_functions.py
+++ new_single_line_support_functions.py
@@ -6,9 +6,13 @@ import numpy as np
 
 
 def mask_color(frame, color):
-
-    white = [[70, 0, 40], [255, 255, 255]]
-    orange = [[0, 0, 50], [15, 255, 255]]
+    # Light Environment
+    white = [[70, 10, 150], [255, 255, 255]]
+    orange = [[1, 10, 60], [15, 200, 220]]
+    
+    # Dark Environment
+    # white = [[70, 0, 40], [255, 255, 255]]
+    # orange = [[0, 0, 50], [15, 255, 255]]
 
     if color=='white':
 
@@ -26,37 +30,53 @@ def mask_color(frame, color):
     return output, mask
 
 
-def steering_angle_calculation(frame, color):
+def steering_angle_calculation(frame, color, value_threshold=200):
     
     masked_image, _ = mask_color(frame, color=color)
     sums = np.sum(masked_image, axis=2)
     height, width = sums.shape
 
     try:
-        (y,x)=np.where(sums>100)
-        # x_max = np.max(x[np.where(y==np.min(y))])
-        # x_min = np.max(x[np.where(y==np.max(y))])
-        y_max = np.max(y) - 30
-        y_min = np.min(y)
+        (y,x)=np.where(sums > value_threshold)
+        
+        # --------------------------------------------------
+        # New condition for when the line has a slope > 45 deg
+        # --------------------------------------------------
+        length_of_top_row = len(x[np.where(y==np.min(y))])
 
-        try:
-            x_min_median = np.median(x[np.where(y==np.min(y))])
-            x_max_median = np.median(x[np.where(y==np.max(y)-30)])
-        except:
-            x_min_median = width/2
-            x_max_median = width/2
+        if length_of_top_row < 20:
+            (x,y)=np.where(sums > value_threshold)
+            flag_end_of_line_on_x = True
+        # --------------------------------------------------
+        # --------------------------------------------------
+
+        x_min_median = np.median(x[np.where(y==np.min(y))])
+        x_max_median = np.median(x[np.where(y==np.max(y)-30)])
         
 
-
-
+        y_max = np.max(y) - 30
+        y_min = np.min(y)
 
 
         y_middle_calc_line = (y_min + y_max)/2
         x_middle_calc_line = (x_min_median + x_max_median)/2
 
-        y_middle_line = height/2
-        x_middle_line = width/2
+        # --------------------------------------------------
+        # New condition for when the line has a slope > 45 deg
+        # --------------------------------------------------
+        if flag_end_of_line_on_x:
+            y_middle_line = width/2
+            x_middle_line = height/2
+        # --------------------------------------------------
+        # --------------------------------------------------
+        else:
+            y_middle_line = height/2
+            x_middle_line = width/2
         
+        
+
+
+
         if x_middle_calc_line - x_middle_line < 0:
             distance_from_center = -np.sqrt((x_middle_calc_line - x_middle_line)**2 + (y_middle_calc_line - y_middle_line)**2)
         if x_middle_calc_line - x_middle_line >= 0:
@@ -66,26 +86,36 @@ def steering_angle_calculation(frame, color):
         relative_distance_from_center = distance_from_center / (width/2)
         # print("relative distance from center", relative_distance_from_center)
             
-        print x_min_median, x_max_median
 
         if x_min_median != x_max_median:
-            steering_angle = np.rad2deg(np.arctan((y_max - y_min)/(x_max_median - x_min_median)))
-        if x_min_median == x_max_median:
+            # --------------------------------------------------
+            # New condition for when the line has a slope > 45 deg
+            # --------------------------------------------------
+            if flag_end_of_line_on_x:
+                steering_angle = np.rad2deg(-np.arctan((x_min_median - x_max_median)/(y_min - y_max)))
+            # --------------------------------------------------
+            # --------------------------------------------------
+            else:
+                steering_angle = np.rad2deg(-np.arctan((y_min - y_max)/(x_min_median - x_max_median)))
+            
+        elif x_min_median == x_max_median:
             steering_angle = 0
 
+        
         if steering_angle < 0:
             corr_steering_angle = -(steering_angle + 90)
-            print 'negatives'
         if steering_angle > 0:
             corr_steering_angle = 90 - steering_angle
-        if steering_angle == 0:
+        elif steering_angle == 0:
             corr_steering_angle = 0
-            print 'here'
 
     except:
         print("no line detected")
         corr_steering_angle = 0
         relative_distance_from_center = 0
+        if flag_end_of_line_on_x:
+            y_min_median = width/2
+            y_max_median = width/2
         x_min_median = width/2
         x_max_median = width/2
         y_min = 0
